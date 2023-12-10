8e73122d3709618311565e10c50409d34ef1339b
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2023-12-09 23:07:33 -0800

    Local storage update

---
+++ localstorage/script.js
@@ -0,0 +1,15 @@
+const people = [
+    { name: "Kyle", age: 28 },
+    { name: "Nick", age: 39 },
+    { name: "Alex", age: 22 },
+    { name: "Kyle", age: 44 },
+
+]
+
+// const a = Object.groupBy(people, person => person.name[0])
+
+// console.log(a)
+
+const a = people.push(1);
+
+console.log(a);
